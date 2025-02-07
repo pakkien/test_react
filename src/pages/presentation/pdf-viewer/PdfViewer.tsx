49ce96c76db4ef88c4db21b2707911c710e5b8bb
PAKKIEN <pakkien18@gmail.com>
2025-02-08 02:03:15 +0800

    add back btn for pdf viewer

--- src/pages/presentation/pdf-viewer/PdfViewer.tsx
+++ src/pages/presentation/pdf-viewer/PdfViewer.tsx
@@ -1,8 +1,10 @@
 import DocViewer, { DocViewerRenderers } from '@cyntler/react-doc-viewer';
 import '@cyntler/react-doc-viewer/dist/index.css';
-import { useLocation } from 'react-router-dom';
+import { useLocation, useNavigate } from 'react-router-dom';
 import PageWrapper from '../../../layout/PageWrapper/PageWrapper';
 import Page from '../../../layout/Page/Page';
+import SubHeader, { SubHeaderLeft, SubheaderSeparator } from '../../../layout/SubHeader/SubHeader';
+import Button from '../../../components/bootstrap/Button';
 
 const PdfViewer = () => {
 	// const docs = [
@@ -11,6 +13,7 @@ const PdfViewer = () => {
 	//     ];
 
 	const location = useLocation();
+  const navigate = useNavigate();
 
 	return (
 		// <DocViewer
@@ -20,6 +23,14 @@ const PdfViewer = () => {
 		// />
 
 		<PageWrapper title='PDF Preview'>
+      <SubHeader>
+				<SubHeaderLeft>
+					<Button color='info' isLink icon='ArrowBack' onClick={() => navigate(-1)}>
+						Back
+					</Button>
+					<SubheaderSeparator />
+					<strong className='fs-5'>PDF View</strong>
+          </SubHeaderLeft></SubHeader>
 			<Page>
 				<DocViewer
 					documents={location.state.files.map((file: any) => ({
